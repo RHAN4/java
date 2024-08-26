a122ed95e96fc56d1cd76a8f50f3a4ac70203d82
Rhana <159554135+RHAN4@users.noreply.github.com>
2024-08-26 08:31:19 -0300

    Add files via upload

---
+++ ExInterface/src/main/java/org/example/Cachorro.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Cachorro implements Animal {
+
+    @Override
+    public String emitirSom() {
+        return "Auau!";
+    }
+}
---
+++ ExInterface/src/main/java/org/example/Gato.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Gato implements Animal {
+
+    @Override
+    public String emitirSom() {
+        return "Miau!";
+    }
+}
